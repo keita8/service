1f2c8502f7aaf1df0f8cf2b601f27a2997f22b7e
keita8 <abdulkabirkeita825@gmail.com>
2023-05-28 22:19:35 +0100

    premiere version

--- ecommerce/apps/products/urls.py
+++ ecommerce/apps/products/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name='index'),
+    path('', views.ProductListView.as_view(), name='main'),
+    path('<slug>/', views.ProductDetailView.as_view(), name='detail'),
+    path('categorie/<int:id>/<slug:categorie_slug>/', views.categorie_products, name='categorie'),
+    
 ]
