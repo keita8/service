0dc7cc2f49540f8890c18b8eff38880b392c4a79
keita8 <abdulkabirkeita825@gmail.com>
2023-05-29 00:36:58 +0100

    premiere version

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -90,6 +90,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "ecommerce.apps.social.context_processors.link",
+                "ecommerce.apps.blog.context_processors.blog_latest"
             ],
         },
     },
