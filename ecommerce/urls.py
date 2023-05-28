dbc43acae4f52f4af77b6909e3283336e4337844
keita8 <abdulkabirkeita825@gmail.com>
2023-05-28 22:01:53 +0100

    premiere version

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,13 +4,40 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+import dj_database_url
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import (handler400, handler403, handler404, handler500)
+from ecommerce.apps.account.views import LoginView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
+
+
+
+
 
 urlpatterns = [
     
-    path('product/', include('ecommerce.apps.products.urls', namespace='products')),
-    path('', include('ecommerce.apps.account.urls', namespace='account')),
-    
+
+    path('accounts/', include('ecommerce.apps.account.urls', namespace='accounts')),
+    path('analytics/', include('ecommerce.apps.analytics.urls', namespace='analytics')),
+    path("checkout/address/", include("ecommerce.apps.addresses.urls", namespace="address")),
+    path('contact/', include('ecommerce.apps.contact.urls', namespace='contact')),
+    path('order/', include('ecommerce.apps.order.urls', namespace='order')),
+    path('billing/', include("ecommerce.apps.billing.urls", namespace="billing")),
+    path('marketing/', include('ecommerce.apps.marketing.urls', namespace='marketing')),
+    path('blog/', include('ecommerce.apps.blog.urls', namespace='blog')),
+    path('about-us/', include('ecommerce.apps.about.urls', namespace='about')),
+    path('shop/', include('ecommerce.apps.shop.urls', namespace='shop')),
+    path('cart/', include('ecommerce.apps.cart.urls', namespace='cart')),
+    path('search/', include('ecommerce.apps.search.urls', namespace='search')),
+    path('tinymce/', include('tinymce.urls')),
     path("admin/", admin.site.urls),
+    path('', include('ecommerce.apps.products.urls', namespace='products')),
+    
     
 ]
 
